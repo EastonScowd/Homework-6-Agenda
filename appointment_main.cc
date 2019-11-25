2dc2a25eef88cd0d171ab876531994971fa93887
Easton Scowden <es686918@ohio.edu>
2019-11-25 17:16:12 -0500

    still need to add operator func

--- appointment_main.cc
+++ appointment_main.cc
@@ -5,12 +5,23 @@
  *  @brief: A program that returns information about an appointment using classes. 
  */
 
+//#include "appointment.h" 
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
 #include <string>
+/*
+
+TO DO:
+boolean?
+second constructor
+split to three files
+
+*/
+
 
 using namespace std;
+
 class Appointment{
 public:
     Appointment(){
@@ -35,7 +46,12 @@ public:
     string getTitle() ;
     void setTitle(string newTitle) ; 
     string getDate() ; 
-    void setDate(int year, int month, int day) ; 
+    void setDate(int year, int month, int day) ;
+
+    string militaryToStandard(int time); 
+    int standardToMilitary(string time);
+    //bool operator ==(const Appointment &first, const Appointment &second);
+
 private:
     string title ; 
     string date ; 
@@ -47,10 +63,7 @@ private:
 } ;
 //function prototypes
 
-int main(int argc, char const *argv[]) {
-    Appointment one ; 
-    cout << one.getDate() ;  
-    
+int main(int argc, char const *argv[]) {  
     return 0;
 }// main
 
@@ -90,7 +103,7 @@ int Appointment::getTime(){
 }
 
 void Appointment::setTime(int newTime){
-    if(newTime >= 0 && newTime <= 1159){
+    if(newTime >= 0 && newTime <= 2400){
         time = newTime ;
     } 
 }
@@ -120,3 +133,90 @@ string Appointment::getDate(){
 
     return y + "-" + m + "-" + d ; 
 }
+
+string Appointment::militaryToStandard(int time){
+    int hours ; 
+    int minutes ; 
+    string minutesString ;  
+    string amPM ; 
+    string standardT ; 
+
+    if(time >= 1300 && time < 2360){
+        hours = (time - 1200) / 100 ; 
+        minutes = (time - 1200) - (hours * 100) ;
+        minutesString = to_string(minutes) ;  
+        amPM = " PM" ; 
+        if(minutes < 10){
+            minutesString = "0" + to_string(minutes) ; 
+        }
+        standardT = to_string(hours) + ":" + minutesString +  amPM ;
+    } else if (time == 2400 || time == 0){
+        standardT = "12:00 AM" ; 
+    } else if(time >= 100 && time < 1160){
+        hours = time / 100 ; 
+        minutes = time - (hours * 100) ; 
+        amPM = " AM" ;
+        minutesString = to_string(minutes) ; 
+        if(minutes < 10){
+            minutesString = "0" + to_string(minutes) ; 
+        }
+        standardT = to_string(hours) + ":" + minutesString +  amPM ;
+    } else if(time == 1200){
+        standardT = "12:00 PM" ; 
+    } else if(time > 1200 && time < 1260) {
+        hours = time / 100 ; 
+        minutes = time - (hours * 100) ; 
+        amPM = " PM" ;
+        minutesString = to_string(minutes) ; 
+        if(minutes < 10){
+            minutesString = "0" + to_string(minutes) ; 
+        }
+        standardT = to_string(hours) + ":" + minutesString +  amPM ;
+    }
+    return standardT ; 
+}
+
+int Appointment::standardToMilitary(string time){
+    string militaryT ; 
+    int returnTime ; 
+    if(time.length() == 7){
+        if(time.substr(5) == "AM"){
+            militaryT += time[0] ;  
+            militaryT += time.substr(2,2) ;
+            returnTime = stoi(militaryT) ;
+        } else if(time.substr(5) == "PM"){
+            militaryT += time[0] ; 
+            militaryT += time.substr(2,2) ;
+            returnTime = stoi(militaryT) + 1200 ;  
+        }
+    }
+    if(time.length() == 8){
+        if(time.substr(0,2) == "12" && time.substr(6) == "AM"){
+            militaryT += time.substr(3,2) ; 
+            returnTime = stoi(militaryT) ; 
+        } else if(time.substr(0,2) == "12" && time.substr(6) == "PM"){
+            militaryT += "12" ; 
+            militaryT += time.substr(3,2) ; 
+            returnTime = stoi(militaryT) ;
+        } else if(time.substr(6) == "AM"){
+        militaryT += time.substr(0,2) ;  
+        militaryT += time.substr(3,2) ;
+        returnTime = stoi(militaryT) ;
+        } else if(time.substr(6) == "PM"){
+            militaryT += time.substr(0,2) ; 
+            militaryT += time.substr(3,2) ;
+            returnTime = stoi(militaryT) + 1200 ;  
+        }
+    }
+    return returnTime ; 
+}
+
+/*bool Appointment::operator ==(const Appointment &first, const Appointment &second){
+    if(first.date == second.date && first.time == second.time && first.title == second.title && first.duration == second.duration){
+        return true ; 
+    }else{
+        return false ;
+    }  
+}
+*/
+
